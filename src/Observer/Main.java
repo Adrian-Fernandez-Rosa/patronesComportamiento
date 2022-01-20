4a740019711dc96eb428944ff668d027f0d5709a
adrianprogama <adrianfr2011@gmail.com>
2022-01-20 13:19:11 -0300

    patron Observer

---
+++ src/Observer/Main.java
@@ -0,0 +1,18 @@
+package Observer;
+
+public class Main {
+    public static void main(String[] args) {
+        Emisora emisora = new Emisora();
+
+
+        ReceptorTV tv = new ReceptorTV();
+        ReceptorRadio radio = new ReceptorRadio();
+        ReceptorSatelite sat = new ReceptorSatelite();
+
+        emisora.meteReceptor(tv);
+        emisora.meteReceptor(radio);
+        emisora.meteReceptor(sat);
+
+        emisora.emite();
+    }
+}
