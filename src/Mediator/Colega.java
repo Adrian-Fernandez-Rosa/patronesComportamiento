bf478b04f3923e77aff29b2432709a05d5c9d4b8
adrianprogama <adrianfr2011@gmail.com>
2022-01-21 17:49:40 -0300

    Correccion clase mediator

--- src/Mediator/Colega.java
+++ src/Mediator/Colega.java
@@ -3,6 +3,9 @@ package Mediator;
 abstract public class  Colega {
     public Mediator mediator;
 
+    public Colega() {
+        System.out.println("Estoy en contructor de clase padre");
+    }
     public void setMediator(Mediator mediator){
         this.mediator = mediator;
     }
--- src/Mediator/ColegaConcreto2.java
+++ src/Mediator/ColegaConcreto2.java
@@ -4,6 +4,12 @@ public class ColegaConcreto2 extends Colega{
 
 
 
+
+    public ColegaConcreto2(){
+        System.out.println("Soy constructor en colegaConcreto2");
+    }
+
+
     @Override
     void recibe() {
         System.out.println("He recibido un mensaje, soy ColegaConcreto 2");
